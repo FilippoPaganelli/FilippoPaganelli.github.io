54bc640da69c4f38503824622726eb867ea54973
FilippoPaganelli <32205866+FilippoPaganelli@users.noreply.github.com>
2021-03-19 11:55:34 +0100

    added clues, needs fixes

--- js/crosswords.js
+++ js/crosswords.js
@@ -1,10 +1,9 @@
 const ROWS = 11
 const COLS = 10
+const DEFAULT_CLUE = '< click on a cell to show clue >'
 var black_tiles = 0
 var guessed = 0
 var toWin = 0
-
-let solutions = [{ "letter": "d", "number": 1 }, { "letter": "i", "number": 2 }, { "letter": "s", "number": 3 }, { "letter": "p", "number": 4 }, { "letter": "e", "number": 5 }, { "letter": "r", "number": 6 }, { "letter": "d", "number": 7 }, { "letter": "e", "number": 8 }, { "letter": "r", "number": "-" }, { "letter": "a", "number": 9 }, { "letter": "-", "number": "-" }, { "letter": "c", "number": 10 }, { "letter": "a", "number": "-" }, { "letter": "l", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "m", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "o", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "d", "number": 11 }, { "letter": "a", "number": 12 }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 13 }, { "letter": "a", "number": "-" }, { "letter": "s", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "i", "number": 14 }, { "letter": "o", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "s", "number": 15 }, { "letter": "s", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": 16 }, { "letter": "t", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "a", "number": 17 }, { "letter": "t", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "p", "number": 18 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": 19 }, { "letter": "-", "number": "-" }, { "letter": "i", "number": 20 }, { "letter": "o", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "i", "number": 21 }, { "letter": "n", "number": "-" }, { "letter": "b", "number": "-" }, { "letter": "a", "number": 22 }, { "letter": "t", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "e", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "l", "number": 23 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "c", "number": 24 }, { "letter": "t", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "s", "number": 25 }, { "letter": "e", "number": "-" }, { "letter": "d", "number": 26 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": 27 }, { "letter": "i", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "o", "number": "-" }, { "letter": "c", "number": 28 }, { "letter": "e", "number": "-" }, { "letter": "c", "number": 29 }, { "letter": "o", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 30 }, { "letter": "a", "number": "-" }, { "letter": "o", "number": 31 }, { "letter": "-", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": 32 }, { "letter": "l", "number": "-" }, { "letter": "i", "number": 33 }, { "letter": "t", "number": "-" }, { "letter": "o", "number": 34 }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 35 }, { "letter": "u", "number": "-" }, { "letter": "m", "number": 36 }, { "letter": "-", "number": "-" }, { "letter": "f", "number": 37 }, { "letter": "r", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "m", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "o", "number": "-" }]
 
 $(function() {
     make_board()
@@ -13,11 +12,12 @@ $(function() {
 make_board = function() {
     var title = ''
     var cell_html = ''
+    var index = 0
     for (var i = 0; i < COLS; i++) {
         $("#crossword").append('<tr id="' + 'row_' + i + '"></tr>')
         for (var j = 0; j < ROWS; j++) {
 
-            var index = i * ROWS + j
+            index = i * ROWS + j
             var sol = solutions[index].letter
             var num = solutions[index].number
 
@@ -32,7 +32,7 @@ make_board = function() {
                     title = num
                 }
 
-                cell_html += '<div onmouseover="setClue(' + index + ')" onclick="setClue(' + index + ')"><input onkeyup="cellHandler(' + index + ')" onfocus="selectCell(' + index + ')" id="' + index +
+                cell_html += '<div  onclick="setClue(' + index + ')"><input onkeyup="focusNextCell(' + index + ')" onfocus="selectCell(' + index + ')" id="' + index +
                     '" maxlength="1" class="input-cell"' + ' title="' + title + '"'
                 'type="text"/></div>'
 
@@ -42,13 +42,41 @@ make_board = function() {
     }
     toWin = COLS * ROWS - black_tiles
     $("#remaing_letters").text(toWin)
+    setDefaultClues()
 }
 
 setClue = function(index) {
-    $("#clue_span").text('cell ' + index)
+    var num = solutions[index].number
+    var clue = ''
+    $("#clue_span_v").text('---')
+    $("#clue_span_h").text('---')
+
+    if (num !== '-')
+        var elem = clues[num - 1]
+
+    if (elem) { // got clues to show
+        console.log(num + ' - ' + index)
+        if (elem.v_clue !== '-') {
+            clue = elem.v_clue
+            $("#clue_span_v").text(clue)
+        } else $("#clue_span_h").text('---')
+        if (elem.h_clue !== '-') {
+            clue = elem.h_clue
+            $("#clue_span_h").text(clue)
+        } else $("#clue_span_v").text('---')
+    } else {
+        setDefaultClues()
+    }
 }
 
-cellHandler = function(index) {
+setDefaultClues = function() {
+    $("#clue_span_v").css({ 'font-style': 'italic' })
+    $("#clue_span_h").css({ 'font-style': 'italic' })
+    $("#clue_span_v").text(DEFAULT_CLUE)
+    $("#clue_span_h").text(DEFAULT_CLUE)
+}
+
+focusNextCell = function(index) {
     var cell = document.getElementById(index)
     var value = cell.value
 
--- js/sol.js
+++ js/sol.js
@@ -1,7 +1,151 @@
-//solutions = [{ "letter": "d", "number": 1 }, { "letter": "i", "number": 2 }, { "letter": "s", "number": 3 }, { "letter": "p", "number": 4 }, { "letter": "e", "number": 5 }, { "letter": "r", "number": 6 }, { "letter": "d", "number": 7 }, { "letter": "e", "number": 8 }, { "letter": "r", "number": "-" }, { "letter": "a", "number": 9 }, { "letter": "-", "number": "-" }, { "letter": "c", "number": 10 }, { "letter": "a", "number": "-" }, { "letter": "l", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "m", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "o", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "d", "number": 11 }, { "letter": "a", "number": 12 }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 13 }, { "letter": "a", "number": "-" }, { "letter": "s", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "i", "number": 14 }, { "letter": "o", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "s", "number": 15 }, { "letter": "s", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": 16 }, { "letter": "t", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "a", "number": 17 }, { "letter": "t", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "p", "number": 18 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": 19 }, { "letter": "-", "number": "-" }, { "letter": "i", "number": 20 }, { "letter": "o", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "i", "number": 21 }, { "letter": "n", "number": "-" }, { "letter": "b", "number": "-" }, { "letter": "a", "number": 22 }, { "letter": "t", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "e", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "l", "number": 23 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "c", "number": 24 }, { "letter": "t", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "s", "number": 25 }, { "letter": "e", "number": "-" }, { "letter": "d", "number": 26 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": 27 }, { "letter": "i", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "o", "number": "-" }, { "letter": "c", "number": 28 }, { "letter": "e", "number": "-" }, { "letter": "c", "number": 29 }, { "letter": "o", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 30 }, { "letter": "a", "number": "-" }, { "letter": "o", "number": 31 }, { "letter": "-", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": 32 }, { "letter": "l", "number": "-" }, { "letter": "i", "number": 33 }, { "letter": "t", "number": "-" }, { "letter": "o", "number": 34 }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 35 }, { "letter": "u", "number": "-" }, { "letter": "m", "number": 36 }, { "letter": "-", "number": "-" }, { "letter": "f", "number": 37 }, { "letter": "r", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "m", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "o", "number": "-" }]
+solutions = [{ "letter": "d", "number": 1 }, { "letter": "i", "number": 2 }, { "letter": "s", "number": 3 }, { "letter": "p", "number": 4 }, { "letter": "e", "number": 5 }, { "letter": "r", "number": 6 }, { "letter": "d", "number": 7 }, { "letter": "e", "number": 8 }, { "letter": "r", "number": "-" }, { "letter": "a", "number": 9 }, { "letter": "-", "number": "-" }, { "letter": "c", "number": 10 }, { "letter": "a", "number": "-" }, { "letter": "l", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "m", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "o", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "d", "number": 11 }, { "letter": "a", "number": 12 }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 13 }, { "letter": "a", "number": "-" }, { "letter": "s", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "i", "number": 14 }, { "letter": "o", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "s", "number": 15 }, { "letter": "s", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": 16 }, { "letter": "t", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "a", "number": 17 }, { "letter": "t", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "p", "number": 18 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": 19 }, { "letter": "-", "number": "-" }, { "letter": "i", "number": 20 }, { "letter": "o", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "i", "number": 21 }, { "letter": "n", "number": "-" }, { "letter": "b", "number": "-" }, { "letter": "a", "number": 22 }, { "letter": "t", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "e", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "l", "number": 23 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "c", "number": 24 }, { "letter": "t", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "s", "number": 25 }, { "letter": "e", "number": "-" }, { "letter": "d", "number": 26 }, { "letter": "a", "number": "-" }, { "letter": "n", "number": 27 }, { "letter": "i", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "o", "number": "-" }, { "letter": "c", "number": 28 }, { "letter": "e", "number": "-" }, { "letter": "c", "number": 29 }, { "letter": "o", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 30 }, { "letter": "a", "number": "-" }, { "letter": "o", "number": 31 }, { "letter": "-", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "-", "number": "-" }, { "letter": "a", "number": 32 }, { "letter": "l", "number": "-" }, { "letter": "i", "number": 33 }, { "letter": "t", "number": "-" }, { "letter": "o", "number": 34 }, { "letter": "-", "number": "-" }, { "letter": "n", "number": 35 }, { "letter": "u", "number": "-" }, { "letter": "m", "number": 36 }, { "letter": "-", "number": "-" }, { "letter": "f", "number": 37 }, { "letter": "r", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "m", "number": "-" }, { "letter": "a", "number": "-" }, { "letter": "r", "number": "-" }, { "letter": "t", "number": "-" }, { "letter": "i", "number": "-" }, { "letter": "n", "number": "-" }, { "letter": "o", "number": "-" }]
 
-/*clues = [
-    {
-
-    }
-]*/
+clues = [{
+    'number': 1,
+    'h_clue': 'sinonimo di "spargerà"',
+    'v_clue': 'tipo di corrente elettrica'
+}, {
+    'number': 2,
+    'h_clue': '-',
+    'v_clue': 'Christian, apprezzato doppiatore italiano'
+}, {
+    'number': 3,
+    'h_clue': '-',
+    'v_clue': 'malattia degenerativa che interessa i muscoli'
+}, {
+    'number': 4,
+    'h_clue': '-',
+    'v_clue': 'vi fu eretta la prima sede della Apple Computer...in breve'
+}, {
+    'number': 5,
+    'h_clue': '-',
+    'v_clue': 'minerale dal colore sanguigno'
+}, {
+    'number': 6,
+    'h_clue': '-',
+    'v_clue': 'brutto mammifero di piccole dimensioni'
+}, {
+    'number': 7,
+    'h_clue': '-',
+    'v_clue': 'condizione fisica che disidrata'
+}, {
+    'number': 8,
+    'h_clue': '-',
+    'v_clue': 'Olmi, regista de "Il mestiere delle armi"'
+}, {
+    'number': 9,
+    'h_clue': '-',
+    'v_clue': 'quando non si può avere, un bambino si...'
+}, {
+    'number': 10,
+    'h_clue': 'si usava per scrivere',
+    'v_clue': '-'
+}, {
+    'number': 11,
+    'h_clue': 'affermazione di Vladimir Putin',
+    'v_clue': '-'
+}, {
+    'number': 12,
+    'h_clue': '-',
+    'v_clue': 'l\'uomo bianco per cui Hitler simpatizzava'
+}, {
+    'number': 13,
+    'h_clue': 'in gergo: "trovata", "scovata"',
+    'v_clue': '-'
+}, {
+    'number': 14,
+    'h_clue': 'la banca della Santa Sede',
+    'v_clue': 'lo siamo "Noi"'
+}, {
+    'number': 15,
+    'h_clue': 'fu un corpo di polizia tedesca',
+    'v_clue': 'si distingue positivamente'
+}, {
+    'number': 16,
+    'h_clue': 'lo sono due che...si piacciono',
+    'v_clue': '-'
+}, {
+    'number': 17,
+    'h_clue': '-',
+    'v_clue': 'Ancona'
+}, {
+    'number': 18,
+    'h_clue': 'un famoso Peter, mai cresciuto',
+    'v_clue': '-'
+}, {
+    'number': 19,
+    'h_clue': '-',
+    'v_clue': 'ciò che lo segue è importante'
+}, {
+    'number': 20,
+    'h_clue': 'una giornata in Campania',
+    'v_clue': '-'
+}, {
+    'number': 21,
+    'h_clue': 'imbattersi in qualcosa...quasi',
+    'v_clue': '-'
+}, {
+    'number': 22,
+    'h_clue': '-',
+    'v_clue': 'anello di filo e comune lombardo'
+}, {
+    'number': 23,
+    'h_clue': 'un tipo di rete',
+    'v_clue': '-'
+}, {
+    'number': 24,
+    'h_clue': 'lo fu Marcello Lippi',
+    'v_clue': '-'
+}, {
+    'number': 25,
+    'h_clue': 'si usano per insaporire il brodo',
+    'v_clue': '-'
+}, {
+    'number': 26,
+    'h_clue': '-',
+    'v_clue': 'se non è "presa" è...'
+}, {
+    'number': 27,
+    'h_clue': '-',
+    'v_clue': 'anonimato che deriva dal Latino'
+}, {
+    'number': 28,
+    'h_clue': 'lingua slava poco diffusa',
+    'v_clue': '-'
+}, {
+    'number': 29,
+    'h_clue': '-',
+    'v_clue': 'l\'automobile che usava Wiston Churchill'
+}, {
+    'number': 30,
+    'h_clue': '"no" detto a Lisbona',
+    'v_clue': '-'
+}, {
+    'number': 31,
+    'h_clue': '-',
+    'v_clue': '"Organizzazione dei Nazionalisti Ucraini"'
+}, {
+    'number': 32,
+    'h_clue': 'ad alcuni puzza, ad altri sa di fresco...',
+    'v_clue': '-'
+}, {
+    'number': 33,
+    'h_clue': '-',
+    'v_clue': '"Istituto Magistrale"'
+}, {
+    'number': 34,
+    'h_clue': '-',
+    'v_clue': '"altrimenti" a Londra'
+}, {
+    'number': 35,
+    'h_clue': 'lo è "42", in breve',
+    'v_clue': '-'
+}, {
+    'number': 36,
+    'h_clue': '-',
+    'v_clue': 'sinonimo gergale di "adesso"'
+}, {
+    'number': 37,
+    'h_clue': 'è cantato come "campanaro"',
+    'v_clue': '-'
+}]
