2e118b1dbd5e14f7a7b9f419bf5e125d8c820ed1
FilippoPaganelli <32205866+FilippoPaganelli@users.noreply.github.com>
2021-04-28 13:39:01 +0200

    solutions updated, errors fixed

--- js/sol.js
+++ js/sol.js
@@ -32,7 +32,7 @@ cw_1_clues = [{
 }, {
     'number': 5,
     'h_clue': '-',
-    'v_clue': 'brutto mammifero di piccole dimensioni'
+    'v_clue': 'piccolo mammifero portatore di malattie'
 }, {
     'number': 6,
     'h_clue': '-',
@@ -40,7 +40,7 @@ cw_1_clues = [{
 }, {
     'number': 7,
     'h_clue': '-',
-    'v_clue': 'Olmi, regista de "Il mestiere delle armi"'
+    'v_clue': 'iniziali di Olmi, regista de "Il mestiere delle armi"'
 }, {
     'number': 9,
     'h_clue': '-',
@@ -68,7 +68,7 @@ cw_1_clues = [{
 }, {
     'number': 33,
     'h_clue': 'fu un corpo di polizia tedesca',
-    'v_clue': 'si distingue positivamente'
+    'v_clue': 'uno che si distingue positivamente, si dice che si __'
 }, {
     'number': 36,
     'h_clue': 'lo sono due che...si piacciono',
@@ -132,7 +132,7 @@ cw_1_clues = [{
 }, {
     'number': 86,
     'h_clue': '-',
-    'v_clue': '"Organizzazione dei Nazionalisti Ucraini"'
+    'v_clue': '"Organizzazione dei Nazionalisti Ucraini", anagramma'
 }, {
     'number': 90,
     'h_clue': 'ad alcuni puzza, ad altri sa di fresco...',
@@ -162,158 +162,4 @@ cw_1_clues = [{
 cw_1 = { 'name': '1 - the first', 'info': cw_1_info, 'sol': cw_1_sol, 'clues': cw_1_clues }
 
 // 2 - cinema
-cw_2_sol = ["d", "i", "s", "p", "e", "r", "d", "e", "r", "a", "-", "c", "a", "l", "a", "m", "a", "i", "o", "-", "d", "a", "-", "n", "a", "s", "a", "t", "a", "-", "i", "o", "r", "s", "s", "-", "a", "t", "t", "r", "a", "t", "t", "i", "p", "a", "d", "-", "i", "o", "r", "n", "a", "t", "a", "i", "n", "t", "a", "t", "-", "e", "-", "l", "a", "n", "c", "t", "-", "s", "e", "d", "a", "n", "i", "-", "o", "c", "e", "c", "o", "-", "a", "-", "n", "a", "o", "-", "a", "-", "a", "l", "i", "t", "o", "-", "n", "u", "m", "-", "f", "r", "a", "m", "a", "r", "t", "i", "n", "o"]
-
-cw_2_info = { 'rows': 10, 'cols': 11 }
-
-cw_2_clues = [{
-    'number': 0,
-    'h_clue': 'sinonimo di "spargerà"',
-    'v_clue': 'tipo di corrente elettrica'
-}, {
-    'number': 1,
-    'h_clue': '-',
-    'v_clue': 'Christian, apprezzato doppiatore italiano'
-}, {
-    'number': 2,
-    'h_clue': '-',
-    'v_clue': 'malattia degenerativa che interessa i muscoli'
-}, {
-    'number': 3,
-    'h_clue': '-',
-    'v_clue': 'vi fu eretta la prima sede della Apple Computer...in breve'
-}, {
-    'number': 4,
-    'h_clue': '-',
-    'v_clue': 'minerale dal colore sanguigno'
-}, {
-    'number': 5,
-    'h_clue': '-',
-    'v_clue': 'brutto mammifero di piccole dimensioni'
-}, {
-    'number': 6,
-    'h_clue': '-',
-    'v_clue': 'condizione fisica che disidrata'
-}, {
-    'number': 7,
-    'h_clue': '-',
-    'v_clue': 'Olmi, regista de "Il mestiere delle armi"'
-}, {
-    'number': 9,
-    'h_clue': '-',
-    'v_clue': 'quando non si può avere, un bambino si...'
-}, {
-    'number': 11,
-    'h_clue': 'si usava per scrivere',
-    'v_clue': '-'
-}, {
-    'number': 20,
-    'h_clue': 'affermazione di Vladimir Putin',
-    'v_clue': '-'
-}, {
-    'number': 21,
-    'h_clue': '-',
-    'v_clue': 'l\'uomo bianco per cui Hitler simpatizzava'
-}, {
-    'number': 23,
-    'h_clue': 'in gergo: "trovata", "scovata"',
-    'v_clue': '-'
-}, {
-    'number': 30,
-    'h_clue': 'la banca della Santa Sede',
-    'v_clue': 'lo siamo "Noi"'
-}, {
-    'number': 33,
-    'h_clue': 'fu un corpo di polizia tedesca',
-    'v_clue': 'si distingue positivamente'
-}, {
-    'number': 36,
-    'h_clue': 'lo sono due che...si piacciono',
-    'v_clue': '-'
-}, {
-    'number': 40,
-    'h_clue': '-',
-    'v_clue': 'Ancona'
-}, {
-    'number': 44,
-    'h_clue': 'Pressione arteriosa diastolica',
-    'v_clue': '-'
-}, {
-    'number': 46,
-    'h_clue': '-',
-    'v_clue': 'fu presidente degli USA, iniziali'
-}, {
-    'number': 48,
-    'h_clue': 'una giornata in Campania',
-    'v_clue': '-'
-}, {
-    'number': 55,
-    'h_clue': 'quasi intatto',
-    'v_clue': '-'
-}, {
-    'number': 58,
-    'h_clue': '-',
-    'v_clue': 'anello di filo e comune lombardo'
-}, {
-    'number': 63,
-    'h_clue': 'un tipo di rete',
-    'v_clue': '-'
-}, {
-    'number': 66,
-    'h_clue': 'lo fu Marcello Lippi',
-    'v_clue': '-'
-}, {
-    'number': 69,
-    'h_clue': 'si usano per insaporire il brodo',
-    'v_clue': '-'
-}, {
-    'number': 71,
-    'h_clue': '-',
-    'v_clue': 'se non è "presa" è...'
-}, {
-    'number': 73,
-    'h_clue': '-',
-    'v_clue': 'anonimato che deriva dal Latino'
-}, {
-    'number': 77,
-    'h_clue': 'lingua slava poco diffusa',
-    'v_clue': '-'
-}, {
-    'number': 79,
-    'h_clue': '-',
-    'v_clue': 'l\'automobile che usava Wiston Churchill'
-}, {
-    'number': 84,
-    'h_clue': '"no" detto a Lisbona',
-    'v_clue': '-'
-}, {
-    'number': 86,
-    'h_clue': '-',
-    'v_clue': '"Organizzazione dei Nazionalisti Ucraini"'
-}, {
-    'number': 90,
-    'h_clue': 'ad alcuni puzza, ad altri sa di fresco...',
-    'v_clue': '-'
-}, {
-    'number': 92,
-    'h_clue': '-',
-    'v_clue': '"Istituto Magistrale"'
-}, {
-    'number': 94,
-    'h_clue': '-',
-    'v_clue': '"altrimenti" a Londra'
-}, {
-    'number': 96,
-    'h_clue': 'lo è "42", in breve',
-    'v_clue': '-'
-}, {
-    'number': 98,
-    'h_clue': '-',
-    'v_clue': 'sinonimo gergale di "adesso"'
-}, {
-    'number': 100,
-    'h_clue': 'è cantato come "campanaro"',
-    'v_clue': '-'
-}]
-
-cw_2 = { 'name': '2 - cinema', 'info': cw_1_info, 'sol': cw_1_sol, 'clues': cw_1_clues }
+// coming soon...
